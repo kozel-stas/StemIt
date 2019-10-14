280038e6a272d5bed537828a6fda566169cdfd37
kozel-stas <stas.kozel9@gmail.com>
2019-10-14 11:03:32 +0300

    Added post and increased heaer size.

--- src/main/java/com/stemmer/rest/StemController.java
+++ src/main/java/com/stemmer/rest/StemController.java
@@ -2,7 +2,11 @@ package com.stemmer.rest;
 
 import com.stemmer.service.StemService;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -22,7 +26,21 @@ public class StemController {
             method = RequestMethod.GET,
             produces = {MediaType.APPLICATION_JSON_VALUE}
     )
-    public Map<String, String> stem(@RequestParam("words") String[] words) {
+    public Map<String, String> stemGet(@RequestParam("words") String[] words) {
+        return stemWords(words);
+    }
+
+    @RequestMapping(
+            value = "/ru",
+            method = RequestMethod.POST,
+            consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE}
+    )
+    public Map<String, String> stemPost(String[] words) {
+        return stemWords(words);
+    }
+
+    private Map<String, String> stemWords(String[] words) {
         Map<String, String> result = new HashMap<>(words.length);
         for (String word : words) {
             result.put(word, ruStemService.stem(word));
